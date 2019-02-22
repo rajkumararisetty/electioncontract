1210bf3edb2bcf3df15a52ded73f7516631705be
rajkumar.arisetty <rajkumararisetty1993@gmail.com>
2019-02-22 07:01:26 +0530

    Boilerplate for development

---
+++ contracts/Election.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.22;
+
+contract Election {
+    // Store Candidate
+    string public candidate;
+    // Read Candidate
+    // Constructor
+    constructor() public {
+        candidate = "Candidate 1";
+    }
+}
